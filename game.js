1930b0262418381e3d9a461811836bb2a8299eb1
AdvaitSarkar <AdvaitSarkar@users.noreply.github.com>
2019-03-29 18:08:33 +0000

    mostly slider maths

--- game.js
+++ game.js
@@ -1,12 +1,10 @@
-let incomeTaxRate = 0
-let researchFunding = 0
-let educationFunding = 0
-
 let stats = {
     "population": 10,
     "educated": 0,
     "productivity": 1,
-    "nationalProductivity": 0,
+	"nationalProductivity": 0,
+	"peopleWealth": 0,
+	"perCapitaWealth": 0,
     "governmentIncome": 0,
     "researchPoints": 0,
     "qualityOfLife": 0
@@ -14,8 +12,11 @@ let stats = {
 
 let levers = {
     "incomeTaxRate": 0.5,
-    "researchFunding": 0.5,
-    "educationFunding": 0.5
+    "researchFunding": 0.20,
+    "educationFunding": 0.20,
+    "healthFunding": 0.20,
+    "environmentFunding": 0.20,
+    "militaryFunding": 0.20
 }
 
 // create stats divs
@@ -37,20 +38,54 @@ forEachPropertyIn(levers, function(id) {
     slider.type = "range"
     slider.min = 0
     slider.max = 1
-    slider.step = 0.1
+    slider.step = 0.01
     slider.value = levers[id]
     slider.className = "form-control-range"
     slider.id = id
-    slider.oninput = function(){
-        label.innerHTML = id + ": " + slider.value    
-    }
-
+    slider.oninput = function(){sliderChanged(id, slider.value, label)}
     label.innerHTML = id + ": " + slider.value
 
     byId("levers").appendChild(label)
     byId("levers").appendChild(slider)
     //refreshResource(id)
 })
+
+function sliderChanged(id, value, label) {
+    label.innerHTML = id + ": " + value
+    if(id==="incomeTaxRate") {}
+        //
+    else {
+        // normalise funding sliders
+        const remainingValue = 1 - parseFloat(byId(id).value)
+        //console.log("remaining: "+remainingValue)
+
+        let ids = ["researchFunding", "educationFunding", "healthFunding", "environmentFunding", "militaryFunding"]
+        let desiredFunds = 0
+        for (let j=0; j<ids.length; j++) {
+            if(ids[j]===id) continue
+            desiredFunds += parseFloat(byId(ids[j]).value)
+        }
+        //console.log("desired: "+desiredFunds)
+
+
+        const unitRemainingFunds = (desiredFunds===0) ? 0 : remainingValue / desiredFunds
+        //console.log("unit: "+unitRemainingFunds)
+        //console.log("---")
+
+
+        let totalAllocatedFunds = 0
+        for (let j=0; j<ids.length; j++) {
+            const i = ids[j]
+            totalAllocatedFunds += parseFloat(byId(i).value)
+            if(i===id) continue
+            else {
+                byId(i).value = parseFloat(byId(i).value)*unitRemainingFunds //(remainingValue / (ids.length-1))// (remainingValue===0) ? 0 : parseFloat(byId(i).value) * (remainingValue / (ids.length-1))
+                byId(i).previousSibling.innerHTML = i+ ": " + byId(i).value
+            }
+        }
+        //console.log(totalAllocatedFunds)
+    }
+}
 
 function step(timestamp) {
     if(timestamp-previousTickTime < 1000) window.requestAnimationFrame(step)
@@ -62,17 +97,20 @@ function step(timestamp) {
         for(i=0; i<elapsedTicks; i++)
         {
             const educatedProportion = (stats.population===0)? 0 : stats.educated/stats.population
-            incomeTaxRate = byId("incomeTaxRate").value
-            researchFunding = byId("researchFunding").value
-            educationFunding = byId("educationFunding").value
+            let incomeTaxRate = parseFloat(byId("incomeTaxRate").value)
+            let researchFunding = parseFloat(byId("researchFunding").value)
+            let educationFunding = parseFloat(byId("educationFunding").value)
 
-            stats.population += (stats.qualityOfLife/100) * stats.population
+            stats.population += Math.min(stats.peopleWealth, (stats.qualityOfLife/100) * stats.population)
             stats.educated = Math.min(stats.population, stats.educated+((educationFunding/100)*stats.population))
             stats.productivity = stats.qualityOfLife
             stats.researchPoints += researchFunding * stats.governmentIncome * Math.max(0.1, educatedProportion)
             stats.qualityOfLife = 1 - incomeTaxRate + educatedProportion
-            stats.nationalProductivity = stats.population * stats.productivity
-            stats.governmentIncome = incomeTaxRate * stats.nationalProductivity
+			stats.nationalProductivity = stats.population * stats.productivity
+			stats.governmentIncome = incomeTaxRate * stats.nationalProductivity
+			stats.peopleWealth += stats.nationalProductivity - stats.governmentIncome - (0.1*stats.population) // 0.1 represents wealth demand per capita
+			stats.perCapitaWealth = stats.peopleWealth / stats.population
+			
         }
 
         // update stats divs
